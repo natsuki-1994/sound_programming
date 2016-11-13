ff1d3ceb572c90a56020997de807fa87744234fb
Natsuki Ikeda <ikeda.natsuki@unipro.co.jp>
2016-11-13 20:08:14 +0900

    tune

--- fft_ifft.py
+++ fft_ifft.py
@@ -4,6 +4,7 @@ import time
 import wave
 import numpy
 import scipy.fftpack
+from scipy.io.wavfile import write
 import matplotlib.pyplot
 from pylab import *
 
@@ -11,18 +12,26 @@ if __name__ == "__main__":
 
     wf = wave.open("sample10.wav", "r")
 
-    n_len = wf.getnframes()
     n_fft = 128
     n_overlap = 2
     n_shift = n_fft / n_overlap
-    fs = wf.getframerate()  # サンプリング周波数
+
+    fs = wf.getframerate()
+    n_len = wf.getnframes()
+
+    print("Channel num : {}[channel]".format(wf.getnchannels()))
+    print("Sample size : {}[byte] {}[bit]".format(wf.getsampwidth(), wf.getsampwidth() * 8))
+    print("Sampling rate : {}[Hz]".format(fs))
+    print("Frame num : {}".format(n_len))
+    print("Params : {}".format(wf.getparams()))
+    print("Sec : {}[sec]".format(float(n_len / fs)))
 
     # 入力バッファ
     xs = wf.readframes(n_len)
     xs = frombuffer(xs, dtype="int16") / 32768.0  # -1から+1に正規化
 
     # 中間バッファ
-    zs = numpy.zeros(n_len)
+    zs = numpy.zeros(n_fft)
     Zs = numpy.zeros(n_fft)
 
     # 出力バッファ
@@ -34,17 +43,17 @@ if __name__ == "__main__":
     # 雑音除去のしきい値
     threshold = 0.5
 
-    # FFT -> IFFT
+    # FFT -> noise cancel -> IFFT
     start_time = time.time()
     for start in range(0, n_len - n_fft, n_shift):
         xs_cut = xs[start: start + n_fft]
         xs_win = xs_cut * window
         Xs = scipy.fftpack.fft(xs_win, n_fft)
+        fft_time = time.time()
 
         # 周波数振幅と周波数位相
         Xs_amplitude_spectrum = numpy.array([numpy.sqrt(c.real ** 2 + c.imag ** 2) for c in Xs])
         Xs_phase_spectrum = numpy.array([numpy.arctan2(c.imag, c.real) for c in Xs])
-        freq_list = scipy.fftpack.fftfreq(n_fft, d=1.0 / fs)
 
         # 信号処理
         Xs_amplitude_spectrum -= threshold  # 周波数振幅からしきい値を減算
@@ -55,18 +64,20 @@ if __name__ == "__main__":
 
         ys[start: start + n_fft] += numpy.real(zs)
     needed_time = time.time() - start_time
-    print("needed time for fft -> ifft (5s data):{}[sec]".format(needed_time))
+    print("needed time for fft -> noise cancel -> ifft (10s data):{}[sec]".format(needed_time))
 
-    # 3秒分プロット
+    # 音楽ファイル書き込み
+    write("sample10_nc.wav", fs, ys)
+
     fig = matplotlib.pyplot.figure(1, figsize=(8, 10))
     ax = fig.add_subplot(211)
-    ax.plot(xs[: fs * 5])
+    ax.plot(xs[: fs * 10])
     ax.set_title("input signal")
     ax.set_xlabel("time [pt]")
     ax.set_ylabel("amplitude")
 
     ax = fig.add_subplot(212)
-    ax.plot(ys[: fs * 5])
+    ax.plot(ys[: fs * 10])
     ax.set_title("output signal")
     ax.set_xlabel("time [pt]")
     ax.set_ylabel("amplitude")
