198b73a2a6057ebbca3f692c94456716d062cbe8
Natsuki Ikeda <ikeda.natsuki@unipro.co.jp>
2016-11-13 20:45:23 +0900

    tune 声の伸張は約0.7s

--- stretch.py
+++ stretch.py
@@ -1,6 +1,6 @@
 # coding:utf-8
 
-import time
+# import time
 import wave
 import numpy
 import scipy.fftpack
@@ -8,58 +8,29 @@ from scipy.io.wavfile import write
 import matplotlib.pyplot
 from pylab import *
 
-if __name__ == "__main__":
 
-    wf = wave.open("sample10.wav", "r")
+def print_wave_information():
+    print("Channel num : {} channel".format(wf.getnchannels()))
+    print("Sample size : {} byte {} bit".format(wf.getsampwidth(), wf.getsampwidth() * 8))
+    print("Sampling rate : {} Hz".format(fs))
+    print("Frame num : {}".format(n_len))
+    print("Params : {}".format(wf.getparams()))
+    print("Sec : {} sec".format(float(n_len / fs)))
 
+
+# FFT -> noise cancel -> IFFT
+def noise_canceling():
     n_fft = 128
     n_overlap = 2
     n_shift = n_fft / n_overlap
 
-    fs = wf.getframerate()
-    n_len = wf.getnframes()
-    n_len_stretch = int(n_len * 1.5) + 1
+    window = numpy.hanning(n_fft)  # 窓関数
+    threshold = 0.5  # 雑音除去のしきい値
 
-    print("Channel num : {}[channel]".format(wf.getnchannels()))
-    print("Sample size : {}[byte] {}[bit]".format(wf.getsampwidth(), wf.getsampwidth() * 8))
-    print("Sampling rate : {}[Hz]".format(fs))
-    print("Frame num : {}".format(n_len))
-    print("Params : {}".format(wf.getparams()))
-    print("Sec : {}[sec]".format(float(n_len / fs)))
-
-    # 入力バッファ
-    xs = wf.readframes(n_len)
-    xs = frombuffer(xs, dtype="int16") / 32768.0  # -1から+1に正規化
-
-    # 中間バッファ
-    zs = numpy.zeros(n_fft)
-    Zs = numpy.zeros(n_fft)
-
-    # 出力バッファ
-    ys = numpy.zeros(n_len)
-
-    # 窓関数
-    window = numpy.hanning(n_fft)
-
-    # 雑音除去のしきい値
-    threshold = 0.5
-
-    # ストレッチ用
-    offset0 = 0
-    offset1 = 0
-    template_size = 441  # fs * 0.01
-    p_min = 220  # fs * 0.005
-    p_max = 882  # fs * 0.02
-
-    # 出力バッファその2
-    ws = numpy.zeros(n_len_stretch)
-
-    # FFT -> noise cancel -> IFFT
     for start in range(0, n_len - n_fft, n_shift):
         xs_cut = xs[start: start + n_fft]
         xs_win = xs_cut * window
         Xs = scipy.fftpack.fft(xs_win, n_fft)
-        fft_time = time.time()
 
         # 周波数振幅と周波数位相
         Xs_amplitude_spectrum = numpy.array([numpy.sqrt(c.real ** 2 + c.imag ** 2) for c in Xs])
@@ -69,13 +40,19 @@ if __name__ == "__main__":
         Xs_amplitude_spectrum -= threshold  # 周波数振幅からしきい値を減算
         Xs_amplitude_spectrum[Xs_amplitude_spectrum < 0.0] = 0.0  # 周波数振幅が負の値を0に修正
 
-        Zs = Xs_amplitude_spectrum * numpy.cos(Xs_phase_spectrum) + Xs_amplitude_spectrum * numpy.sin(Xs_phase_spectrum) * 1j
-        zs = scipy.fftpack.ifft(Zs, n_fft)
+        Z_temp = Xs_amplitude_spectrum * numpy.cos(Xs_phase_spectrum) + Xs_amplitude_spectrum * numpy.sin(Xs_phase_spectrum) * 1j
+        z_temp = scipy.fftpack.ifft(Z_temp, n_fft)
 
-        ys[start: start + n_fft] += numpy.real(zs)
+        ys[start: start + n_fft] += numpy.real(z_temp)
 
-    # stretch music file
-    start_time = time.time()
+
+def stretch_voice():
+    offset0 = 0
+    offset1 = 0
+    template_size = 441  # fs * 0.01
+    p_min = 220  # fs * 0.005
+    p_max = 882  # fs * 0.02
+
     while offset0 + p_max * 2 < n_len:
         al = ys[offset0: offset0 + template_size]
         r_max = 0.0
@@ -100,12 +77,10 @@ if __name__ == "__main__":
 
         offset0 += 2 * p
         offset1 += 3 * p
-    needed_time = time.time() - start_time
-    print("needed time for stretch music file (10s data):{}[sec]".format(needed_time))
 
-    # 音楽ファイル書き込み
-    write("sample10_nc_stretch.wav", fs, ws)
 
+# ノイズキャンセルの結果をグラフ表示
+def plot_result_noise_canceling():
     fig = matplotlib.pyplot.figure(1, figsize=(8, 10))
     ax = fig.add_subplot(211)
     ax.plot(xs[: fs * 10])
@@ -120,3 +95,29 @@ if __name__ == "__main__":
     ax.set_ylabel("amplitude")
 
     matplotlib.pyplot.show()
+
+
+if __name__ == "__main__":
+
+    wf = wave.open("sample10.wav", "r")
+
+    # 入出力ファイル長
+    fs = wf.getframerate()
+    n_len = wf.getnframes()  # 入力ファイル長
+    n_len_stretch = int(n_len * 1.5) + 1  # 出力ファイルは入力ファイルの1.5倍
+
+    # noise canceling 入出力バッファ
+    xs = wf.readframes(n_len)
+    xs = frombuffer(xs, dtype="int16") / 32768.0  # -1から+1に正規化
+    ys = numpy.zeros(n_len)
+
+    # stretch voice 出力バッファ
+    ws = numpy.zeros(n_len_stretch)
+
+    print_wave_information()
+    noise_canceling()
+    stretch_voice()
+    plot_result_noise_canceling()
+
+    # 音楽ファイル書き込み
+    write("sample10_nc_stretch.wav", fs, ws)
